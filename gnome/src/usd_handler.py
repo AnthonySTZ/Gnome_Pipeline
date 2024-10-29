52b578adc256e6794ba71d71c7f0e237492a980d
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-29 20:06:51 +0100

    add usd sublayers and save usd stage

--- gnome/src/usd_handler.py
+++ gnome/src/usd_handler.py
@@ -1,12 +1,27 @@
 import os
-from pxr import Usd, UsdGeom
+from pxr import Usd, UsdGeom, Sdf
 
 
-def create_new_stage(path: str, name: str) -> Usd.Stage:
+def create_new_stage(path: str, name: str) -> tuple[Usd.Stage, str]:
     """Create a new USD stage with the given path and name"""
 
     os.makedirs(path, exist_ok=True)
     if not (name.endswith(".usd") or name.endswith(".usda") or name.endswith(".usdc")):
         name += ".usda"
-    stage: Usd.Stage = Usd.Stage.CreateNew(os.path.join(path, name))
-    return stage
+    path = os.path.join(path, name)
+    stage: Usd.Stage = Usd.Stage.CreateNew(path)
+    return stage, path
+
+
+def save_stage(stage: Usd.Stage) -> None:
+    """Save the USD stage"""
+    stage.GetRootLayer().Save()
+
+
+def add_layer(root_layer: Sdf.Layer, sub_layer_path: str) -> Sdf.Layer:
+    """Add a new root layer to the USD stage"""
+    sub_layer: Sdf.Layer = Sdf.Layer.FindOrOpen(sub_layer_path)
+    if not sub_layer:
+        sub_layer: Sdf.Layer = Sdf.Layer.CreateNew(sub_layer_path)
+    root_layer.subLayerPaths.append(sub_layer.identifier)
+    return sub_layer
