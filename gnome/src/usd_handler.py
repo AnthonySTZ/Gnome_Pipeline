e3bb59c0789da701aaae66c426df161fb60b2325
AnthonySTZ <anthonycandy2309@gmail.com>
2024-10-29 19:37:29 +0100

    create new stage

--- gnome/src/usd_handler.py
+++ gnome/src/usd_handler.py
@@ -1,1 +1,12 @@
+import os
 from pxr import Usd, UsdGeom
+
+
+def create_new_stage(path: str, name: str) -> Usd.Stage:
+    """Create a new USD stage with the given path and name"""
+
+    os.makedirs(path, exist_ok=True)
+    if not (name.endswith(".usd") or name.endswith(".usda") or name.endswith(".usdc")):
+        name += ".usda"
+    stage: Usd.Stage = Usd.Stage.CreateNew(os.path.join(path, name))
+    return stage
